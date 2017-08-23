48d27c411bcae308fdc36ff89adf88033786984e
Wes <wesleysprague@yahoo.com>
2017-08-23 03:06:47 +0000

    styel errors and partials

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -15,7 +15,7 @@ class ArticlesController < ApplicationController
     def create 
       @article =Article.new(article_params)
       if @article.save
-        flash[:notice] = "article was succesfully created"
+        flash[:success] = "article was succesfully created"
         redirect_to article_path(@article)
       else
         render 'new'
@@ -25,7 +25,7 @@ class ArticlesController < ApplicationController
     def update
       
       if @article.update(article_params)
-        flash[:notice] = "Article was updated"
+        flash[:success] = "Article was updated"
         redirect_to article_path(@article)
       else
         render 'edit'
@@ -37,9 +37,8 @@ class ArticlesController < ApplicationController
     end 
     
     def destroy
-     
       @article.destroy
-      flash[:notice] = "Article was #{@article.title} deleted"
+      flash[:danger] = "Article was #{@article.title} deleted"
       redirect_to articles_path
     end
     private
