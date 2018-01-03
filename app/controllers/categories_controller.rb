c69f689bb346842014d2208fca195d77d20f3f84
BaseballApp <wesleysprague@yahoo.com>
2018-01-03 23:58:02 +0000

    add admin requirements to create categoires

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,6 @@
 class CategoriesController < ApplicationController
+  before_action :require_admin, except: [:index, :show]
+  
   def index 
     @categories = Category.paginate(page: params[:page], per_page: 5)
   end 
@@ -8,7 +10,6 @@ class CategoriesController < ApplicationController
     if @category.save
       flash[:sucess] = "Category was created successfully"
       redirect_to categories_path
-      
     else 
       render 'new'
     end 
@@ -16,7 +17,6 @@ class CategoriesController < ApplicationController
   
   def new
     @category = Category.new
-    
   end
   
   def show
@@ -29,4 +29,10 @@ class CategoriesController < ApplicationController
   params.require(:category).permit(:name)
   end
   
+  def require_admin
+    if !logged_in? || (logged_in? and !current_user.admin?)
+      flash[:danger] = "Only admins can perform that action"
+      redirect_to categories_path
+    end
+  end   
 end 
